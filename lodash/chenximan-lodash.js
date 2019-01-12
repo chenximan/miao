fe499368de59bffdc5368a6c7edc8b6cf6dcfeff
chenximan <15320015211@163.com>
2019-01-12 20:26:59 +0800

    125

--- lodash/chenximan-lodash.js
+++ lodash/chenximan-lodash.js
@@ -1,15 +1,33 @@
 var chenximan = function(){
-
-    function chunk(ary, size = 1) {
-  
+  chunk: function(ary,size) {
+    let n = Math.floor(ary.length / size)
+    if(size === 0 || n === 0)
+      return ary
+    let result = []
+    let count = -1
+    for(let i=1; i<=n; i++) {
+      count++
+      result.push([])
+      for(let j=i*size-size; j<i*size; j++) {
+        result[count].push(ary[j])
+      }
     }
-    function compact(ary) {
-  
+    let last = ary.length % size
+    for(let i=1; i<=last; i++) {
+      count++
+      result.push([])
+      result[count].unshift(ary[ary.length-i])
     }
-  
-    return {
-      chunk: chunk,
-      compact: compact,
+    return result
+  },
+  compact: function(ary) {
+    let result = []
+    for(let i=0; i<ary.length; i++) {
+      if(ary[i]) {
+        result.push(ary[i])
+      }
     }
-  }()
+    return result
+  },
+}
   
