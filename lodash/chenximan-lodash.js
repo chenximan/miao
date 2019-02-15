ff4cd2814c762f862a6307907d957ed83d44cf73
chenximan <15320015211@163.com>
2019-02-15 21:28:21 +0800

    zx

--- lodash/chenximan-lodash.js
+++ lodash/chenximan-lodash.js
@@ -1,33 +1,8 @@
 var chenximan = {
-  chunk: function(ary,size) {
-    let n = Math.floor(ary.length / size)
-    if(size === 0 || n === 0)
-      return ary
-    let result = []
-    let count = -1
-    for(let i=1; i<=n; i++) {
-      count++
-      result.push([])
-      for(let j=i*size-size; j<i*size; j++) {
-        result[count].push(ary[j])
-      }
-    }
-    let last = ary.length % size
-    for(let i=1; i<=last; i++) {
-      count++
-      result.push([])
-      result[count].unshift(ary[ary.length-i])
-    }
-    return result
+  identity: value => value,
+  isArray: value => Object.prototype.toString.call(value) === '[object Array]',
+  isObject: value => {
+    if(typeof value === 'function') return true
+    return (Array.isArray(value) || Object.prototype.toString.call(value) === '[object Object]')
   },
-  compact: function(ary) {
-    let result = []
-    for(let i=0; i<ary.length; i++) {
-      if(ary[i]) {
-        result.push(ary[i])
-      }
-    }
-    return result
-  },
-  isArray: value => Object.prototype.toString.call(value) === "[object Array]",
 },
